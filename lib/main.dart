7d6a82f3eb29130ec40e72fe96972f268dc65631
alanlopezromero <alopezromero192@gmail.com>
2025-03-02 00:08:29 -0600

    Mi primer commit con todos los cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mongo_aplication/screens/main_screen.dart';
 import 'package:mongo_aplication/screens/phones_screen.dart';
 import 'package:mongo_aplication/services/mongo_service.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +18,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PhoneScreen(),
+      home: MainScreen(),
     );
   }
 }
